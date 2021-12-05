5500894352f6a941d88c71e134257c262a748493
hypnotic2402 <Saket.Gurjar@iiitb.ac.in>
2021-12-05 23:19:07 +0530

    TCP handshake complete

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+
+// http.globalAgent.maxSockets = 20;
+// http.get({host: 'localhost', port:'3000' , path:'/api' , agent: false} , callback);
 
 app.listen(3000 , ()=>{
     console.log('App listening on port 3000');
 });
 app.use(express.json({limit:'100mb'}));
 app.use(express.static('public'));
+
+// API for client to change variables
+var lights="OFF";
 app.post('/api' , (req, res)=>{
-    console.log(req.body);
+    lights = req.body.ls;
+    console.log(lights);
+    res.json({status: 'success'});
 });
+
+// API for raspberry pi to connect to
+
+app.get('/rasp/lights' , (req, res)=>{
+    res.send(lights);
+});
+
