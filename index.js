6f5e039cbf4f0211f2b7708cafc1ad59631f5314
hypnotic2402 <Saket.Gurjar@iiitb.ac.in>
2021-12-05 22:44:37 +0530

    Sending 1 var to server

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ const app = express();
 app.listen(3000 , ()=>{
     console.log('App listening on port 3000');
 });
-
+app.use(express.json({limit:'100mb'}));
 app.use(express.static('public'));
+app.post('/api' , (req, res)=>{
+    console.log(req.body);
+});
